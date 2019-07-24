9deab88deec8f5e6a79cf42684f3dbed46f7ce17
Shark <muerbingsha@126.com>
2019-07-24 15:33:17 +1000

    Add files via upload

---
+++ BusinessLogic1.java
@@ -0,0 +1,7 @@
+// source: http://www.java2s.com/Tutorials/Java/JUnit/0140__JUnit_Parameterized_Test.htm
+
+public class BusinessLogic1 {
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
